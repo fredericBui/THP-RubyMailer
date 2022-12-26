7aa712babda56e0c52f5df07640eec68eadfb482
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-26 10:38:13 +0100

    mailer config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
 # Load the Rails application.
 require_relative "application"
+require 'dot_env'
+current_environment = DotEnv.get_environment
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SMTP_LOGIN'],
+  :password => ENV['SMTP_PSW'],
+  :address => 'smtp.mailtrap.io',
+  :domain => 'smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
